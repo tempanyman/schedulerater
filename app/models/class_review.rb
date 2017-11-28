1f86211842f9cd64a4b52a989bb827781617f2a6
Allen Tong <allentong29@gmail.com>
2017-11-28 01:52:38 -0800

    Rebased, schedule_reviews/new loads

--- app/models/class_review.rb
+++ app/models/class_review.rb
@@ -2,6 +2,6 @@ class ClassReview < ApplicationRecord
   belongs_to :schedule_review
   belongs_to :user
   belongs_to :course
-  accepts_nested_attributes_for :courses
+  accepts_nested_attributes_for :course
   serialize :prereqs, Array
 end
