0b75129e4d2ffc15f1a0a56e197ec8d25aa91d2e
Allen Tong <allentong29@gmail.com>
2017-11-27 17:11:23 -0800

    Merged allen branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users#, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:index, :show]
   resources :schedule_reviews, only: [:show, :new, :edit, :create, :update, :destroy]
   resources :class_reviews
