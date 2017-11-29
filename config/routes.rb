28357d6e273880ca0c82de573d456543d2e3b241
Allen Tong <allentong29@gmail.com>
2017-11-28 23:29:35 -0800

    Removed unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :schedule_reviews, only: [:index, :show, :new, :edit, :create, :update, :destroy]
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :class_reviews
-  resources :courses
+  resources :class_reviews, only: [:create, :update, :destroy]
+  resources :courses, only: [:index, :show]
   get '/search/:search_term', to: 'class_reviews#search', as: 'search'
   post '/search', to: 'class_reviews#search_redirect', as: 'search_redirect'
 end
